e4ee4c140fde8661b0122083609b80bbe0365525
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-07-04 19:18:17 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let excusas = () => {
+  let who = ["El perro", "Mi abuela", "Su tortuga", "Mi pájaro"];
+  let action = ["comió", "orinó", "aplastó", "rompió"];
+  let what = ["mi tarea", "la llave", "el auto"];
+  let when = [
+    "antes de clase",
+    "hace un momento",
+    "cuando terminé",
+    "durante mi almuerzo",
+    "mientras yo estaba orando"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let whoIndice = Math.floor(Math.random() * who.length);
+  let actionIndice = Math.floor(Math.random() * action.length);
+  let whatIndice = Math.floor(Math.random() * what.length);
+  let whenIndice = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndice] +
+    " " +
+    action[actionIndice] +
+    " " +
+    what[whatIndice] +
+    " " +
+    when[whenIndice] +
+    "."
+  );
+};
+
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = excusas();
 };
